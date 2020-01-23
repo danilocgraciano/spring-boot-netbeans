027e22323ebb1e42b6bc74507505e226fbcda848
Danilo C. Graciano <danilocgraciano@gmail.com>
2020-01-23 16:51:41 -0300

    Commit Inicial

---
+++ DemoProject/src/main/java/com/example/demo/view/HelloController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author danilo
+ */
+@Controller
+public class HelloController {
+    
+    @GetMapping({"/", "/hello"})
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
